b1eb8ca82b19d74674204503b69ad88a87db6e0d
Seth Kellas <sjkellas@gmail.com>
2019-09-02 17:17:15 -0400

    Update webpack to run via dev-server

---
+++ src/components/Initiative.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface InitiativeProps {};
+
+export class Initiative extends React.Component<InitiativeProps, {}> {
+    render() {
+        return <h1>Initiative!</h1>;
+    }
+}
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Initiative } from './components/Initiative';
+
+ReactDOM.render(
+  <Initiative/>, document.getElementById("example")  
+);
